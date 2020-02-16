756432ddb5007626d2a6c58457aeb7d07a1a102b
zhq <zhq199622@163.com>
2020-02-16 17:50:32 +0800

    F4 更新 context 里的值

--- demo2/src/F1.js
+++ demo2/src/F1.js
@@ -2,21 +2,29 @@ import React, { Component } from 'react'
 const NContext = React.createContext();
 
 class F1 extends React.Component {
-    constructor(){
+    constructor() {
         super()
-        this.state={
-            n:1000
+        this.state = {
+            n: 1000,
+            authority: () => {
+                this.setState({
+                    ...this.state,
+                    n: 2000
+                })
+            }
         }
-        setTimeout(()=>{
+        this.change=()=>{
             this.setState({
-                n:99
+                ...this.state,
+                n: 3000
             })
-        },3000)
+        }
     }
     render() {
         return (
-            <NContext.Provider value={this.state.n}>
+            <NContext.Provider value={this.state}>
                 <F2 />
+                <button onClick={this.change}>F1 change n</button>  
             </NContext.Provider>
         );
     }
@@ -34,6 +42,7 @@ function F4(props) {
     return (
         <div>
             {props.n4}
+            <button onClick={props.authority}>F4 change n</button>
         </div>
     );
 }
@@ -42,7 +51,7 @@ class F3 extends React.Component {
     render() {
         return (
             <NContext.Consumer>
-                {n => <F4 n4={n} />}
+                {state => <F4 n4={state.n} authority={state.authority} />}
             </NContext.Consumer>
         )
     }
